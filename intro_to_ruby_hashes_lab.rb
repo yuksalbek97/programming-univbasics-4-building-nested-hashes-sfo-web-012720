288a1610ba87326176f74c9fb69d6d5137604404
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 05:00:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_third_tier
 	                  :b_and_o_railroad  =>
 	                  {
 	                  },
-	                    :shortline =>
+	                    :shortline_railroad =>
 	                    {
 	                      
 	                    }
