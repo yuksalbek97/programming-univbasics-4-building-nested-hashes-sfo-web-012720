b971fb0a7bf837fcbbad64843abe6c6e31cfe624
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 04:59:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 	      :pieces => 4,
 	        :rent_in_dollars => 
 	        {
-	          :one_pieces_owned => 25,
+	          :one_piece_owned => 25,
 	          :two_pieces_owned => 50,
 	          :three_pieces_owned => 100,
 	          :four_pieces_owned => 200
