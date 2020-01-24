d0c35e0fc9d4afd242ac5d69139c68ca61876639
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-24 05:05:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -104,7 +104,7 @@ def monopoly_with_fourth_tier
 	                  },
 	                    :shortline_railroad =>
 	                    {
-	                      
+	                      "mortgage_value" => "$800"
 	                    }
 	           
 	                    
